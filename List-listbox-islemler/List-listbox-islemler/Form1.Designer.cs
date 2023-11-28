01263e34cbb40b8f03b6b44163eaadec68a5f726
DilanKaya <145452308+DilanKaya2013@users.noreply.github.com>
2023-11-28 14:19:13 +0300

    Add files via upload

---
+++ List-listbox-islemler/List-listbox-islemler/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace List_listbox_islemler
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblAd = new System.Windows.Forms.Label();
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnListele = new System.Windows.Forms.Button();
+            this.btnTemizle = new System.Windows.Forms.Button();
+            this.btnElemansayisi = new System.Windows.Forms.Button();
+            this.btnSirala = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lblAd
+            // 
+            this.lblAd.AutoSize = true;
+            this.lblAd.BackColor = System.Drawing.Color.Tomato;
+            this.lblAd.Font = new System.Drawing.Font("Ravie", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAd.Location = new System.Drawing.Point(12, 15);
+            this.lblAd.Name = "lblAd";
+            this.lblAd.Size = new System.Drawing.Size(136, 22);
+            this.lblAd.TabIndex = 0;
+            this.lblAd.Text = "Öğrenci Adı:";
+            // 
+            // txtAd
+            // 
+            this.txtAd.Font = new System.Drawing.Font("Ravie", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAd.ForeColor = System.Drawing.Color.LightSeaGreen;
+            this.txtAd.Location = new System.Drawing.Point(154, 12);
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(118, 29);
+            this.txtAd.TabIndex = 1;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.BackColor = System.Drawing.Color.Tomato;
+            this.btnEkle.Font = new System.Drawing.Font("Ravie", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEkle.ForeColor = System.Drawing.Color.Black;
+            this.btnEkle.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnEkle.Location = new System.Drawing.Point(294, 10);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(151, 39);
+            this.btnEkle.TabIndex = 2;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = false;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // btnListele
+            // 
+            this.btnListele.BackColor = System.Drawing.Color.Tomato;
+            this.btnListele.Font = new System.Drawing.Font("Ravie", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnListele.ForeColor = System.Drawing.Color.Black;
+            this.btnListele.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnListele.Location = new System.Drawing.Point(294, 57);
+            this.btnListele.Name = "btnListele";
+            this.btnListele.Size = new System.Drawing.Size(151, 39);
+            this.btnListele.TabIndex = 3;
+            this.btnListele.Text = "Listele";
+            this.btnListele.UseVisualStyleBackColor = false;
+            this.btnListele.Click += new System.EventHandler(this.btnListele_Click);
+            // 
+            // btnTemizle
+            // 
+            this.btnTemizle.BackColor = System.Drawing.Color.Tomato;
+            this.btnTemizle.Font = new System.Drawing.Font("Ravie", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTemizle.ForeColor = System.Drawing.Color.Black;
+            this.btnTemizle.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnTemizle.Location = new System.Drawing.Point(462, 10);
+            this.btnTemizle.Name = "btnTemizle";
+            this.btnTemizle.Size = new System.Drawing.Size(151, 39);
+            this.btnTemizle.TabIndex = 4;
+            this.btnTemizle.Text = "Temizle";
+            this.btnTemizle.UseVisualStyleBackColor = false;
+            this.btnTemizle.Click += new System.EventHandler(this.btnTemizle_Click);
+            // 
+            // btnElemansayisi
+            // 
+            this.btnElemansayisi.BackColor = System.Drawing.Color.Tomato;
+            this.btnElemansayisi.Font = new System.Drawing.Font("Ravie", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnElemansayisi.ForeColor = System.Drawing.Color.Black;
+            this.btnElemansayisi.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnElemansayisi.Location = new System.Drawing.Point(462, 57);
+            this.btnElemansayisi.Name = "btnElemansayisi";
+            this.btnElemansayisi.Size = new System.Drawing.Size(151, 39);
+            this.btnElemansayisi.TabIndex = 5;
+            this.btnElemansayisi.Text = "Eleman Sayısı";
+            this.btnElemansayisi.UseVisualStyleBackColor = false;
+            // 
+            // btnSirala
+            // 
+            this.btnSirala.BackColor = System.Drawing.Color.Tomato;
+            this.btnSirala.Font = new System.Drawing.Font("Ravie", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSirala.ForeColor = System.Drawing.Color.Black;
+            this.btnSirala.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnSirala.Location = new System.Drawing.Point(637, 9);
+            this.btnSirala.Name = "btnSirala";
+            this.btnSirala.Size = new System.Drawing.Size(151, 39);
+            this.btnSirala.TabIndex = 6;
+            this.btnSirala.Text = "Sırala";
+            this.btnSirala.UseVisualStyleBackColor = false;
+            // 
+            // btnSil
+            // 
+            this.btnSil.BackColor = System.Drawing.Color.Tomato;
+            this.btnSil.Font = new System.Drawing.Font("Ravie", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSil.ForeColor = System.Drawing.Color.Black;
+            this.btnSil.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnSil.Location = new System.Drawing.Point(637, 57);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(151, 39);
+            this.btnSil.TabIndex = 7;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = false;
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.Tomato;
+            this.listBox1.Font = new System.Drawing.Font("Ravie", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.ForeColor = System.Drawing.Color.Black;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 22;
+            this.listBox1.Location = new System.Drawing.Point(294, 137);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(151, 268);
+            this.listBox1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.txtAd);
+            this.Controls.Add(this.lblAd);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnSirala);
+            this.Controls.Add(this.btnElemansayisi);
+            this.Controls.Add(this.btnTemizle);
+            this.Controls.Add(this.btnListele);
+            this.Controls.Add(this.btnEkle);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblAd;
+        private System.Windows.Forms.TextBox txtAd;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnListele;
+        private System.Windows.Forms.Button btnTemizle;
+        private System.Windows.Forms.Button btnElemansayisi;
+        private System.Windows.Forms.Button btnSirala;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ List-listbox-islemler/List-listbox-islemler/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace List_listbox_islemler
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<string> list = new List<string>();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            string ad = txtAd.Text;
+            list.Add(ad);
+            listBox1.Text = "🦅✨🎉🙂('◡')";
+        }
+
+        private void btnListele_Click(object sender, EventArgs e)
+        {
+            listBox1.DataSource = list.ToList();
+        }
+
+        private void btnTemizle_Click(object sender, EventArgs e)
+        {
+
+            string ad = txtAd.Text;
+            list.Clear();
+            listBox1.DataSource = list.ToList();
+        }
+    }
+}
